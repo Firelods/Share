f089e714f869feff52bb523475d53ddee7bb8aab
Clement LEFEVRE <clement.lefevre1@etu.univ-cotedazur.fr>
2023-01-12 21:06:58 +0100

    testPWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -51,8 +51,6 @@ import { environment } from '../environments/environment';
     MatProgressSpinnerModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
     })
   ],
