01c46a6f37d965aec774c7658390c762b28bbd1e
Clement LEFEVRE <clement.lefevre1@etu.univ-cotedazur.fr>
2023-01-16 19:04:08 +0100

    correction request service

--- src/app/service/request.service.ts
+++ src/app/service/request.service.ts
@@ -4,7 +4,7 @@ import { Injectable, isDevMode } from '@angular/core';
 })
 export class RequestService {
   // make url 'http://localhost:8080/api/' when dev mode and 'https:share-api.clement-lefevre.fr' when prod mode
-  url = isDevMode() ? 'http://localhost:8080/api/' : 'https://share-api.clement-lefevre.fr/';
+  url = isDevMode() ? 'http://localhost:8080/api/' : 'https://share-api.clement-lefevre.fr/api/';
   constructor() { }
 
 }
