7a8d2ce84d15737d2a9c36648766d2938b95a5ff
Clement LEFEVRE <clement.lefevre1@etu.univ-cotedazur.fr>
2023-01-16 18:25:20 +0100

    mettre l'inscription possible dans la page de connexion #1 changement du requestURL en fonction du devMode()

--- src/app/service/request.service.ts
+++ src/app/service/request.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable, isDevMode } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
 export class RequestService {
-  url = 'http://localhost:8080/api/';
+  // make url 'http://localhost:8080/api/' when dev mode and 'https:share-api.clement-lefevre.fr' when prod mode
+  url = isDevMode() ? 'http://localhost:8080/api/' : 'https://share-api.clement-lefevre.fr/';
   constructor() { }
 
 }
